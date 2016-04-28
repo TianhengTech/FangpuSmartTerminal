1700ab579dc316bcc2ffb9794b74be1e7108c125
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-28 17:37:59 +0800

    Nhibernate配置

---
+++ fangpu_terminal_200_snap7/fangpu_terminal/Ultility/Nhibernate/FangpuHibernateGroup.cs
@@ -0,0 +1,52 @@
+﻿using NHibernate.Mapping;
+using System;
+using FluentNHibernate.Mapping;
+
+namespace fangpu_terminal.Ultility.Nhibernate
+{
+    public class warn_info
+    {
+        public virtual int idwarn_info { get; set; }
+        public virtual string device_name { get; set; }
+        public virtual string warn_message { get; set; }
+        public virtual DateTime storetime { get; set; }
+    }
+
+    public class historydata
+    {
+        public virtual int historydataid { set; get; }
+        public virtual string deviceid { set; get; }
+        public virtual string value { set; get; }
+        public virtual float shuayou_consume_seconds { set; get; }
+        public virtual float kaomo_consume_seconds { set; get; }
+        public virtual float kaoliao_consume_seconds { set; get; }
+        public virtual float lengque_consume_seconds { set; get; }
+        public virtual float jinliao_consume_seconds { set; get; }
+        public virtual float kaomo_temp { set; get; }
+        public virtual float kaoliao_temp { set; get; }
+        public virtual float cycletime { set; get; }
+        public virtual DateTime storetime { set; get; }
+        public virtual string systus { set; get; }
+    }
+
+
+    public class user_info
+    {
+        public virtual int iduser { get; set; }
+        public virtual string username { get; set; }
+        public virtual string password { get; set; }
+    }
+
+
+    public class warn_infoMapping : ClassMap<warn_info>
+    {
+        public warn_infoMapping()
+        {
+            Table("warn_info");
+            Id<int>("idwarn_info").GeneratedBy.Identity();
+            Map(m => m.device_name).Nullable();
+            Map(m => m.storetime).Nullable();
+            Map(m => m.warn_message).Nullable();
+        }
+    }
+}
